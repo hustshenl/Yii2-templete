d72673cbb8ee3c3650495b6c296b562387aa2d40
Shen.L <shen@shenl.com>
2015-05-05 22:29:05 +0800

    项目初始化

---
+++ src/common/components/services/Comic.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @Author shen@shenl.com
+ * @Create Time: 2015/5/5 21:02
+ * @Description:
+ */
+
+namespace common\components\service;
+
+use yii\base\Component;
+use yii;
+
+class Comic extends Component {
+
+}
